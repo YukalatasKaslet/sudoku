114285a0cf84e42da4078bd2b854ae34838c4a2e
codeamac09 <codeamac09@gmail.com>
2016-04-04 18:20:56 -0500

    Impresión integer

--- sudoku.rb
+++ sudoku.rb
@@ -1,9 +1,6 @@
 
 
 class Sudoku 
-  attr_reader :sudoku_string
-  attr_reader :sudoku_original
-  attr_reader :aux
 
   private
 
@@ -11,7 +8,8 @@ class Sudoku
     # x = sudoku_string.scan(/./)
     @sudoku_string = sudoku_string
     @sudoku_original = sudoku_string.scan(/./)
-  end#mtd
+  end#mtd initialize
+
 
 
   def board_int
@@ -23,15 +21,112 @@ class Sudoku
       end
     end
      a = Array.new(9) { b.pop(9) }
-  end#mtd
+  end#mtd board_int
 
-  public
+
+
+  # def cuadro_faltantes(array, fila, columna)
+  #   presentes = []
+  #   faltantes = []
+
+  #   for i in 0..3
+  #     for j in 0..3
+  #       for k in 1..9
+  #         if k == array[fila][columna]
+  #           presentes << k
+  #         end#if
+  #       end#each numbers
+  #       columna += 1
+  #     end# for j
+  #     fila += 1
+  #   end#for i
+
+  #   for i in 1..9
+  #     if !( presentes.include?(i) )
+  #       faltantes << i
+  #     end#if
+  #   end#for i
+  #   faltantes
+  # end#mtd cuadrado_faltantes
+
+
+  # def fila_faltantes(array, fila)
+  #   presentes_fila = []
+  #   presentes_3 = []
+  #   posibles_fila = []
+  #   faltantes = []
+
+  #   for i in 0..3
+  #     for j in 0..8
+  #       for k in 1..9
+  #         if k == array[fila][j]
+  #           presentes_fila << k
+  #         end#if
+  #       end#for k
+  #       if presentes_fila.empty?
+  #         presentes_fila << ["."]
+  #       end#if p_fila empty?
+  #       if ( ( j == 2 ) || ( j == 5 ) ) || ( j == 8 )
+  #         presentes_3 << presentes_fila
+  #       end#if
+  #       presentes_fila = []
+  #     end#for j
+  #     fila += 1
+  #   end#for i
+
+  #   presentes_3.each do |a|
+  #     a.each do |n|
+  #       if n.is_a? String
+  #         for k in 1..9
+  #           posibles_fila << k
+  #         end
+  #       else
+  #         for k in 1..9
+  #           if !( n.include?(k) )
+  #             posibles_fila << k
+  #           end#if include
+  #         end#for k
+  #       end#if is_a?
+        
+  #     end#each a
+
+  #     posibles_fila.each do |fila|
+  #       fila
+      
+
+
+
+  # end#mtd fila_faltantes
+
+  # def columna_faltantes(array, columna)
+  # end#mtd columna_faltantes
+
+
+  # def solve
+  #   for i in 0..9
+  #     for j in 0..9
+  #       if ( i % 3 == 0) && ( j % 3 == 0 )
+  #         c_faltantes = cuadro_faltantes(board_int, i, j)
+  #       end#if
+  #     end#for j
+  #   end#for i
+
+  # end#mtd solve
+
+
+  public #----------------------------------------
+
+
+
+
 
   def board
     a = board_int
     count = 0
+    p "---------------------------"
     a.each do |arreglo|
-      if count % 3 == 0 
+      if ( count % 3 == 0 ) && ( count != 0)
+
         p "--------+---------+--------"
       end#if      
       arreglo.each_with_index do |elemento, index|
@@ -43,9 +138,9 @@ class Sudoku
       printf "\n"
       count += 1
     end#f each
-    p "--------+---------+--------"
+    p "---------------------------"
     printf "\n"
-  end#mtd
+  end#mtd board
 
 end#class
 
